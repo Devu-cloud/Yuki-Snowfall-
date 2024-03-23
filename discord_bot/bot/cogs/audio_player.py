571d2bf567e90702a60d098f8e47ba5e2ea25534
Devendra <80771758+Devu-cloud@users.noreply.github.com>
2024-03-23 21:03:21 +0530

    Update audio_player.py

--- discord_bot/bot/cogs/audio_player.py
+++ discord_bot/bot/cogs/audio_player.py
@@ -1,15 +1,1 @@
-import discord
-from discord.ext import commands
 
-class audio_player(commands.Cog):
-
-    def __init__(self, bot):
-        self.bot = bot
-        
-    @commands.Cog.listener()
-    async def on_ready(self):
-      print(f"audio_player Cog is ready:")
-      
-         
-async def setup(bot):
-    await bot.add_cog(audio_player(bot))
